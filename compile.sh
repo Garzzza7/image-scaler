3b3fb7800790a53edad6c5d9006dd1ea393a8b72
Garzzza7 <piotr.czapela@gmail.com>
2025-03-18 22:22:16 +0100

    add zig to compilation script

--- compile.sh
+++ compile.sh
@@ -8,6 +8,11 @@ elif command -v clang++ 2>&1 >/dev/null
 then
     echo "clang++ detected!!!"
     clang++ -g -Ofast -Wall -pedantic -std=c++0x -pthread -static src/main.cpp -o image-scaler
+elif command -v zig 2>&1 >/dev/null
+then
+    echo "zig detected!!!"
+    zig c++ -g -Ofast -Wall -pedantic -std=c++0x -pthread -static src/main.cpp -o image-scaler
 else
+
     echo "No compiler detected!!!"
 fi
