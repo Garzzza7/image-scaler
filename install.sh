6e6aaa6da2bbde4384eee2aebb86cf83e0cb2c59
Garzzza7 <piotr.czapela@gmail.com>
2025-03-18 14:09:46 +0100

    make install script detect debian-based vs arch-based

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install g++ cmake make
+if [ -f "/etc/arch-release" ]; then
+    echo "Arch detected!!!!!!"
+    sudo pacman -S cmake make gcc
+elif [ -f "/etc/debian_version" ]; then
+    echo "Debian detected!!!!!!"
+    sudo apt-get update
+    sudo apt-get install g++ cmake make
+fi
